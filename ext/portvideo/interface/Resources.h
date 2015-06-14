08a092433dd15f8ef637e5127114c5e76f84d8d7
Martin Kaltenbrunner <martin@tuio.org>
2015-06-14 12:05:33 +0200

    fix camera image

--- ext/portvideo/interface/Resources.h
+++ ext/portvideo/interface/Resources.h
@@ -1,16 +1,16 @@
 /*  portVideo, a cross platform camera framework
  Copyright (C) 2005-2015 Martin Kaltenbrunner <martin@tuio.org>
- 
+
  This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
  License as published by the Free Software Foundation; either
  version 2.1 of the License, or (at your option) any later version.
- 
+
  This library is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  Lesser General Public License for more details.
- 
+
  You should have received a copy of the GNU Lesser General Public
  License along with this library; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -32,7 +32,6 @@ extern unsigned char camera[3910];
 //extern Uint8 *getMask();
 //extern SDL_Surface *getIcon();
 extern SDL_Surface *getCamera();
-extern SDL_Rect camera_rect;
 #ifdef __cplusplus
 }
 #endif
